8bcda8c5bc897806fd567bed7585fd883b5e1e6d
leandroalbero <leandro80gb@gmail.com>
2019-09-30 18:56:11 +0200

    -Implemented stage two (Symbol-level correction code), problem is that Hyperskill does not like this solution.

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -4,21 +4,56 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static char[] triple(char[] input){
+        StringBuilder out=new StringBuilder("");
+        for (char c : input) {
+            out.append(String.valueOf(c).repeat(3));
+        }
+        return out.toString().toCharArray();
+    }
+    static char[] scram(char[] input){
         Random r = new Random();
         String alphabet = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRTUVWXYZ";
-        Scanner sc = new Scanner(System.in);
-        char[] ogData = sc.nextLine().toCharArray();
         int lowerBound=0;
         int upperBound=2;
         while(true) {
             try{
-                ogData[lowerBound + r.nextInt((upperBound - lowerBound) + 1)] = alphabet.charAt(r.nextInt(alphabet.length())) ;
+                input[lowerBound + r.nextInt((upperBound - lowerBound) + 1)] = alphabet.charAt(r.nextInt(alphabet.length())) ;
                 lowerBound+=3;upperBound+=3;
             }catch (Exception e){
                 break;
             }
         }
-        System.out.println(ogData);
+        return input;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] input = sc.nextLine().toCharArray();
+        //System.out.println(input);
+        System.out.println(triple(input));
+        char[] scrambled = scram(triple(input));
+        System.out.println(scrambled);
+        char[] ecc=errorCorrect(scrambled);
+        System.out.println(ecc);
+    }
+    static char[] errorCorrect(char[] input){
+        int lowerBound=0;
+        StringBuilder out = new StringBuilder("");
+        while(true) {
+            try {
+                if (input[lowerBound] == input[lowerBound + 1]) {
+                    out.append(input[lowerBound]);
+                } else if (input[lowerBound] == input[lowerBound+2]) {
+                    out.append(input[lowerBound]);
+                } else {
+                    out.append(input[lowerBound + 1]);
+                }
+                lowerBound += 3;
+            }catch (Exception e){
+                break;
+            }
+        }
+        return(out.toString().toCharArray());
     }
 }
