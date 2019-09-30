69693543c03afdb574dd6a35478f371ad0ffe3b1
leandroalbero <leandro80gb@gmail.com>
2019-09-30 16:26:02 +0200

    -Implemented stage one (Symbol-level error emulator)

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,7 +1,24 @@
 package correcter;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Random r = new Random();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRTUVWXYZ";
+        Scanner sc = new Scanner(System.in);
+        char[] ogData = sc.nextLine().toCharArray();
+        int lowerBound=0;
+        int upperBound=2;
+        while(true) {
+            try{
+                ogData[lowerBound + r.nextInt((upperBound - lowerBound) + 1)] = alphabet.charAt(r.nextInt(alphabet.length())) ;
+                lowerBound+=3;upperBound+=3;
+            }catch (Exception e){
+                break;
+            }
+        }
+        System.out.println(ogData);
     }
 }
