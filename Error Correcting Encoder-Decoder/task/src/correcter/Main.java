850be1610e0e8c59feb15b4278cb67e7b7f75c59
leandroalbero <leandro80gb@gmail.com>
2019-10-03 12:49:23 +0200

    -Stage 4 started

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,7 +1,5 @@
 package correcter;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
@@ -17,14 +15,16 @@ public class Main {
         }
         return out.toString().toCharArray();
     }
-    public static String readFileAsString(String fileName) throws IOException {
+
+    private static String readFileAsString(String fileName) throws IOException {
         return new String(Files.readAllBytes(Paths.get(fileName)));
     }
-    static char[] readFile() throws IOException {
-        try{
-            String input = readFileAsString("send.txt");
+
+    private static char[] readFile() throws IOException {
+        try {
+            String input = readFileAsString("/Users/leandroalbero/IdeaProjects/Error Correcting Encoder-Decoder/Error Correcting Encoder-Decoder/task/send.txt");
             return input.toCharArray();
-        }catch (IOException e){
+        } catch (IOException e) {
             return null;
         }
     }
@@ -66,48 +66,76 @@ public class Main {
         return (out.toString().toCharArray());
     }
 
-    public static void main(String[] args) throws IOException {
-        char[] input = readFile();
-        try{
-            FileWriter fw = new FileWriter("received.txt",false);
-            char[] tmp = bitLevelError(input);
-            StringBuilder sb = new StringBuilder();
-            for (int i =0;i<tmp.length;i++){
-                sb.append(tmp[i]);
-            }
-
-            fw.append(sb.toString());
-            fw.close();
-        }catch (java.io.IOException e){
-            System.out.println("FHE");
-        }
-        System.out.println();
-    }
-    static char[] bitLevelError(char[] input){
+    private static char[] bitLevelError(char[] input) {
         String[] hex = new String[input.length];
         Random r1 = new Random();
-        for (int i=0;i<input.length;i++){
-            hex[i]=Integer.toBinaryString(input[i]);
+        for (int i = 0; i < input.length; i++) {
+            hex[i] = Integer.toBinaryString(input[i]);
             int ranPos = r1.nextInt(hex[i].length());
             StringBuilder tmp = new StringBuilder();
             tmp.append(hex[i]);
-            if(tmp.charAt(ranPos)=='0'){
-                tmp.setCharAt(ranPos,'1');
-            }else if(tmp.charAt(ranPos)=='1'){
-                tmp.setCharAt(ranPos,'0');
-            }else{
+            if (tmp.charAt(ranPos) == '0') {
+                tmp.setCharAt(ranPos, '1');
+            } else if (tmp.charAt(ranPos) == '1') {
+                tmp.setCharAt(ranPos, '0');
+            } else {
                 System.out.println();
             }
-            hex[i]=tmp.toString();
+            hex[i] = tmp.toString();
         }
         StringBuilder sb = new StringBuilder();
-        for (int i=0;i<hex.length;i++){
-            sb.append((char)Integer.parseInt(hex[i],2));
+        for (String s : hex) {
+            sb.append((char) Integer.parseInt(s, 2));
         }
         char[] out = new char[sb.length()];
-        for (int i=0;i<sb.length();i++){
-            out[i]=sb.charAt(i);
+        for (int i = 0; i < sb.length(); i++) {
+            out[i] = sb.charAt(i);
         }
         return out;
+    }
+
+    static String[] stringToHex (char[] input){
+        String[] hex =new String[input.length];
+        for (int i=0;i<input.length;i++){
+            hex[i]=String.format("%x",(int)input[i]);
+        }
+        return hex;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Write a mode: ");
+        String mode = sc.nextLine();
+        if (mode.equals("encode")) { //Read sent.txt, convert to rts form and save into encoded.txt
+            char[] input = readFile();
+            System.out.print("\nsend.txt:\ntext view: ");
+            System.out.println(input);
+            System.out.print("hex view: ");
+            String[] hex = stringToHex(input);
+            for (String s:hex) {
+                System.out.print(s+' ');
+            }
+            System.out.println("encoded.txt: ");
+
+
+        } else if (mode.equals("send")) { //Read encoded.txt and simulate errors, then save into received.txt
+
+        } else if (mode.equals("decode")) { //Read from received.txt and save normal text into decoded.txt
+
+        }
+        try {
+            char[] input = readFile();
+            FileWriter fw = new FileWriter("received.txt", false);
+            char[] tmp = bitLevelError(input);
+            StringBuilder sb = new StringBuilder();
+            for (char c : tmp) {
+                sb.append(c);
+            }
+            fw.append(sb.toString());
+            fw.close();
+        } catch (java.io.IOException e) {
+            System.out.println("FHE");
+        }
+        System.out.println();
     }
 }
